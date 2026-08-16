b029d8a953dee4fc61072917fa773652c869506c
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-16 16:30:49 +0530

    Add Java Solution to Patterns/Sliding Window

--- LeetCode/Patterns/Sliding Window/3090. Maximum Length Substring With Two Occurrences/Solution.java
+++ LeetCode/Patterns/Sliding Window/3090. Maximum Length Substring With Two Occurrences/Solution.java
@@ -1,5 +1,25 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        return s.length()/2;
+        int max=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i+1;j<=s.length();j++){
+                HashMap<Character,Integer> map=new HashMap<>();
+                boolean valid=true;
+                String str=s.substring(i,j);
+                for(int k=0;k<str.length();k++){
+                    char ch=str.charAt(k);
+                    int count=map.getOrDefault(ch,0);
+                    if(count>=2){
+                        valid=false;
+                        break;
+                    }
+                    map.put(ch,count+1);
+                }
+                if(valid){
+                    max=Math.max(max,str.length());
+                }
+            }
+        }
+        return max;
     }
 }
