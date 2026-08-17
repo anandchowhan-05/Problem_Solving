f73e59feb5e9a32972853c7e3c81d442f4e59bc0
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-18 00:22:45 +0530

    Add Java Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/3819. Rotate Non Negative Elements/Solution.java
+++ LeetCode/Patterns/Arrays/3819. Rotate Non Negative Elements/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         int n=nums.length;
         int count=0;
         for(int i=0;i<n;i++){
-            if(nums[i]>0){
+            if(nums[i]>=0){
                 count++;
             }
         }
