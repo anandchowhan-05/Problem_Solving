51008ab348215c638144544f4120879b59a5f2fd
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-18 00:21:57 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/4171. Rotate Non Negative Elements/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] rotateElements(int[] nums, int k) {
+        int n=nums.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                count++;
+            }
+        }
+        if(count==0) return nums;
+        int[] b=new int[count];
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                b[j++]=nums[i];
+            }
+        }
+        int[] c=new int[count];
+        k=k%count;
+        for(int i=k;i<count;i++){
+            c[i-k]=b[i];
+        }
+        for(int i=0;i<k;i++){
+            c[count-k+i]=b[i];
+        }
+        int z=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                nums[i]=c[z++];
+            }
+        }
+        return nums;
+    }
+}
