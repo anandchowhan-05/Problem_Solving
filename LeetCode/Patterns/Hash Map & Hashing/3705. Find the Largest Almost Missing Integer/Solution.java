398913f7a66968128911102fcd119648d3a2b8cd
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-18 12:52:55 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/3705. Find the Largest Almost Missing Integer/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/3705. Find the Largest Almost Missing Integer/Solution.java
@@ -1,35 +1,29 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-    //   HashMap<Integer,Integer> map=new HashMap<>();
-       int n=nums.length;
-    //   for(int i=0;i<nums.length/2;i++){
-    //     if(i < k){
-    //         map.put(nums[i],i+1);
-    //         map.put(nums[n-1-i],i+1);
-    //     }else{
-    //       map.put(nums[i],k);
-    //       map.put(nums[n-1-i],k);  
-    //     }
-    //   }
-    //   if(map.containsKey(1)){
-    //     return map.get(1);
-    //   }
-    //   return -1;  
-    if(n<=2 || nums[0]== nums[n-1]) return -1; 
-    int max=Math.max(nums[0],nums[n-1]);
-    HashSet<Integer> set=new HashSet<>();
-    for(int i=1;i<n-1;i++){
-        set.add(nums[i]);
-    }
-    if(set.contains(nums[0]) && set.contains(nums[n-1])){
-        return -1;
-    }else if(set.contains(nums[0])){
-        return nums[n-1];
-    }else if(set.contains(nums[n-1])){
-        return nums[0];
-    }else{
-        return max;
-    }
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        // Every window of size k
+        for (int i = 0; i <= n - k; i++) {
+            HashSet<Integer> seen = new HashSet<>();
+
+            for (int j = i; j < i + k; j++) {
+                seen.add(nums[j]);
+            }
+
+            for (int x : seen) {
+                map.put(x, map.getOrDefault(x, 0) + 1);
+            }
+        }
+
+        int ans = -1;
+
+        for (int x : map.keySet()) {
+            if (map.get(x) == 1) {
+                ans = Math.max(ans, x);
+            }
+        }
+
+        return ans;
     }
 }
