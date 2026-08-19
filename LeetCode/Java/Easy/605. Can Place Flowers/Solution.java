651d242d06be5bdbdfdfe4d9f68e68952cadde96
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-20 00:21:11 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/605. Can Place Flowers/Solution.java
+++ LeetCode/Java/Easy/605. Can Place Flowers/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-
+        if (n == 0) return true;
         for (int i = 0; i < flowerbed.length; i++) {
 
             if (flowerbed[i] == 0 &&
