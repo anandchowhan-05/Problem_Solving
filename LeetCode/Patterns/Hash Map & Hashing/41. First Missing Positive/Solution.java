92f82cc9216286092e3dc1fe0adf2cfc33771d94
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-20 15:44:42 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/41. First Missing Positive/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/41. First Missing Positive/Solution.java
@@ -4,7 +4,7 @@ class Solution {
        for(int i=0;i<nums.length;i++){
             set.add(nums[i]);
        }
-       for(int i=1;i<=nums.length;i++){
+       for(int i=1;i<=nums.length+1;i++){
         if(!set.contains(i)){
             return i;
         }
