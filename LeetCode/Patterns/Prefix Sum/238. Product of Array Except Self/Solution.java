b187ff79627b7cad8735e32ba79e06f789859fa0
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-20 13:22:37 +0530

    Add Java Solution to Patterns/Prefix Sum

--- LeetCode/Patterns/Prefix Sum/238. Product of Array Except Self/Solution.java
+++ LeetCode/Patterns/Prefix Sum/238. Product of Array Except Self/Solution.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n=nums.length;
-       int[] a=new int[n];
-       int k=0;
-       while(k<n){
-        int prod=1;
-        for(int i=0;i<n;i++){
-            if(k==i){
-                int l=1;
-            }else{
-                prod=prod*nums[i];
-            }
+        int n = nums.length;
+        int[] ans = new int[n];
+        ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
         }
-        a[k++]=prod;
-       } 
-       return a;
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * suffix;
+            suffix = suffix * nums[i];
+        }
+        return ans;
     }
 }
