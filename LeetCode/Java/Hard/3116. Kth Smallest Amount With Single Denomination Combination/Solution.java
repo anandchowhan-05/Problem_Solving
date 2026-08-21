b8fc5d76503acd27c6569ea68674ca6417f5de4f
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-21 12:44:01 +0530

    Add Hard Java Solution from LeetCode

---
+++ LeetCode/Java/Hard/3116. Kth Smallest Amount With Single Denomination Combination/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long findKthSmallest(int[] coins, int k) {
+        TreeSet<Long> set = new TreeSet<>();
+        for(int i=1;i<=k;i++){
+            for(int j=0;j<coins.length;j++){
+                set.add((long)coins[j]*i);
+            }
+        }
+        Iterator<Long> it = set.iterator();
+        for(int i1 = 1; i1 < k; i1++) {
+            it.next();
+        }
+        return it.next();
+    }
+}
