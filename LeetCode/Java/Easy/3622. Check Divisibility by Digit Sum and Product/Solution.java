7b35c08a9a395d3f0e8eed5854809c7de5155a62
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-22 10:59:44 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/3622. Check Divisibility by Digit Sum and Product/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int sum=0;
+        int prod=1;
+        int temp=n;
+        while(n!=0){
+            int d=n%10;
+            sum=sum+d;
+            prod = prod*d;
+            n=n/10;
+        }
+        int res=sum+prod;
+        return res%temp==0;
+    }
+}
