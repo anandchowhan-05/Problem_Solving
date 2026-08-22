7b10f6c28ec5d31476c9db3c12f3d3d3513d8db2
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-22 16:54:03 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/1128. Number of Equivalent Domino Pairs/Solution.java
+++ LeetCode/Java/Easy/1128. Number of Equivalent Domino Pairs/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numEquivDominoPairs(int[][] dominoes) {
         int count=0;
-        for(int i=0;i<dominoes.length;i++){
+        for(int i=0;i<dominoes.length-1;i++){
             for(int j=i+1;j<dominoes.length;j++){
                 if((dominoes[i][0]==dominoes[j][0] && dominoes[i][1]==dominoes[j][1] ) || (dominoes[i][0]==dominoes[j][1] && dominoes[i][1]==dominoes[j][0])){
                     count++;
