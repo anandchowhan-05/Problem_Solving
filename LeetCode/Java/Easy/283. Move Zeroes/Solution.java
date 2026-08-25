226535fef7ce0b0ad160879a57d7bba1dc832539
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-25 22:25:57 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/283. Move Zeroes/Solution.java
+++ LeetCode/Java/Easy/283. Move Zeroes/Solution.java
@@ -1,14 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int p1 = 0;
-
-        for (int p2 = 0; p2 < nums.length; p2++) {
-            if (nums[p2] != 0) {
-                int temp = nums[p1];
-                nums[p1] = nums[p2];
-                nums[p2] = temp;
-
-                p1++;
+        int j=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return;
+        for(int i=j+1;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
             }
         }
     }
