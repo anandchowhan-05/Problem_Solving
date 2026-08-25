af18d517f64874dcf6d523d25ded34f9962c2006
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-08-25 22:40:29 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/1137. N-th Tribonacci Number/Solution.java
+++ LeetCode/Java/Easy/1137. N-th Tribonacci Number/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         dp[0]=0;
         dp[1]=1;
         dp[2]=1;
-        for(int i=3;i<37;i++){
+        for(int i=3;i<=37;i++){
             dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         }
         return dp[n];
