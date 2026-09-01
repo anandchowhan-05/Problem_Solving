a9c571b61ed1c1af1d15d3b0b17a642ddf928534
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-09-01 16:29:56 +0530

    Add Easy Java Solution for 4410. Count Integers Appearing in a Single Block from LeetCode

--- LeetCode/Java/Easy/4410. Count Integers Appearing in a Single Block/Solution.java
+++ LeetCode/Java/Easy/4410. Count Integers Appearing in a Single Block/Solution.java
@@ -1,15 +1,38 @@
 class Solution {
     public int countSpecialIntegers(int[] nums) {
-        if(nums.length==1) return 1;
-        int count=0;
-        for(int i=1;i<=100;i++){
-            for(int j=0;j<nums.length-1;j++){
-                if((nums[j]==nums[j+1]) && nums[j]==i){
-                    count++;
+        int count = 0;
+
+        for (int x = 1; x <= 100; x++) {
+            int first = -1;
+            int last = -1;
+            int occurrences = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == x) {
+                    if (first == -1) {
+                        first = i;
+                    }
+                    last = i;
+                    occurrences++;
+                }
+            }
+
+            if (first == -1) continue;
+
+            boolean special = true;
+
+            for (int i = first; i <= last; i++) {
+                if (nums[i] != x) {
+                    special = false;
                     break;
                 }
             }
+
+            if (special) {
+                count++;
+            }
         }
+
         return count;
     }
 }
