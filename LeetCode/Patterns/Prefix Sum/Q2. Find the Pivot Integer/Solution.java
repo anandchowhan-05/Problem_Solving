bcf18c598b4ab1565e0f9b262aac230fcdb5b617
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-09-03 17:28:32 +0530

    Add Easy Java Solution for Q2. Find the Pivot Integer from LeetCode

---
+++ LeetCode/Patterns/Prefix Sum/Q2. Find the Pivot Integer/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int tot=(n*(n+1))/2;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum +=i;
+            if((tot+i-sum) == sum) return i;
+        }
+        return -1;
+    }
+}
