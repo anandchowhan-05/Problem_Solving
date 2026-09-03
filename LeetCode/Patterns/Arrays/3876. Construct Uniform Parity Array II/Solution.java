59b334d6eed308c7db62d887f10d3ac893610354
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-09-03 17:19:37 +0530

    Add Medium Java Solution for 3876. Construct Uniform Parity Array II from LeetCode

--- LeetCode/Patterns/Arrays/3876. Construct Uniform Parity Array II/Solution.java
+++ LeetCode/Patterns/Arrays/3876. Construct Uniform Parity Array II/Solution.java
@@ -1,35 +1,23 @@
 class Solution {
-    public boolean uniformArray(int[] nums1) {
-        int n = nums1.length;
-        int[][] temp = new int[n][2];
+    public boolean uniformArray(int[] nums) {
+        int minOdd = Integer.MAX_VALUE;
 
-        if (n == 1)
-            return true;
-
-        for (int i = 0; i < n; i++) {
-            if (nums1[i] % 2 == 0) temp[i][0] = 1;
-            if (nums1[i] % 2 != 0) temp[i][1] = 1;
-            else {
-                for (int j = 0; j < n; j++) {
-                    if (j == i)
-                        continue;
-                    if ((nums1[i] - nums1[j]) % 2 != 0  && (nums1[i] - nums1[j]) >=1 ) {
-                        temp[i][1] = 1;
-                        break;
-                    }
-                }
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                minOdd = Math.min(minOdd, num);
             }
         }
 
-        int even = 0, odd = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (even == 0 && temp[i][0] == 0)
-                even = 1;
-            if (odd == 0 && temp[i][1] == 0)
-                odd = 1;
+        if (minOdd == Integer.MAX_VALUE) {
+            return true; // all even
         }
 
-        return even == 0 || odd == 0;
+        for (int num : nums) {
+            if (num % 2 == 0 && num <= minOdd) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
