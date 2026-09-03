48185617a4ee20d1bed7c6c2b97ad7babc8fbead
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-09-03 17:31:07 +0530

    Add Easy Java Solution for 9. Palindrome Number from LeetCode

--- LeetCode/Java/Easy/9. Palindrome Number/README.md
+++ LeetCode/Java/Easy/9. Palindrome Number/README.md
@@ -8,8 +8,8 @@
 Math
 
 ### 🚀 Performance
-- **Runtime:** 5 ms
-- **Memory:** 43.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Java/Easy/9. Palindrome Number/Solution.java
+++ LeetCode/Java/Easy/9. Palindrome Number/Solution.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-     if(x<0){
-        return false;
-     }  
-     int original=x; 
-     int re=0;
-     while(x!=0){
-        int d=x%10;
-        re =re*10+d;
-        x /=10;
-     }
-     return original==re;
+        if(x<0) return false;
+        int temp=x;
+        int ans=0;
+        while(x!=0){
+            int d=x%10;
+            ans=ans*10+d;
+            x /=10;
+        }
+        return ans==temp;
     }
 }
