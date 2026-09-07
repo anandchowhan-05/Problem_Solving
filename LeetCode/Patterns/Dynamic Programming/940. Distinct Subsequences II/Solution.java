dc60b6fa49cdb2a311dcb4de875544d36d83df2e
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-09-07 11:15:30 +0530

    Add Hard Java Solution for 940. Distinct Subsequences II from LeetCode

--- LeetCode/Patterns/Dynamic Programming/940. Distinct Subsequences II/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/940. Distinct Subsequences II/Solution.java
@@ -1,5 +1,6 @@
 class Solution {
     public int distinctSubseqII(String s) {
+        int mod=1000000007;
         int n=s.length();
         int ans=1;
         int prev=1;
@@ -7,9 +8,9 @@ class Solution {
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
             int index= ch - 'a';
-            ans = ans*2-last[index];
-            last[index]=prev;
-            prev=ans;
+            ans = (ans*2-last[index])%mod;
+            last[index]=prev%mod;
+            prev=ans%mod;
         }
         return ans-1;
     }
