d368292e79c02485788b11c3cc3291bf9daca369
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-09-11 22:33:22 +0530

    Add Easy Java Solution for 3483. Unique 3-Digit Even Numbers from LeetCode

---
+++ LeetCode/Patterns/Hash Map & Hashing/3483. Unique 3-Digit Even Numbers/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        Set<Integer> set = new HashSet<>();
+
+        int n = digits.length;
+
+        for (int i = 0; i < n; i++) {
+
+            // Last digit must be even
+            if (digits[i] % 2 != 0) {
+                continue;
+            }
+
+            for (int j = 0; j < n; j++) {
+
+                if (i == j) {
+                    continue;
+                }
+
+                for (int k = 0; k < n; k++) {
+
+                    // First digit cannot be 0
+                    // And cannot reuse a digit
+                    if (digits[k] == 0 || k == i || k == j) {
+                        continue;
+                    }
+
+                    int num = digits[k] * 100
+                            + digits[j] * 10
+                            + digits[i];
+
+                    set.add(num);
+                }
+            }
+        }
+
+        return set.size();
+    }
+}
