b292a1fc63649433c37d617508384873262c7b3b
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-09-18 00:00:34 +0530

    Add Medium Java Solution for 2139. Minimum Moves to Reach Target Score from LeetCode

---
+++ LeetCode/Patterns/Greedy/2139. Minimum Moves to Reach Target Score/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+        int count=0;
+        while(target != 0){
+            if(target%2==0 && maxDoubles > 0){
+                target=target/2;
+                count++;
+                maxDoubles--;
+            }else{
+                target=target-1;
+                count++;
+            }
+        }
+        int x=target-1;
+        return count+x;
+    }
+}
