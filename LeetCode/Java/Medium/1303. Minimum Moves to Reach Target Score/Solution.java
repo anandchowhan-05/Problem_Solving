504b26211e611f2e1046d558fcc4acfd9d202dde
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-09-18 00:05:56 +0530

    Add Medium Java Solution for 1303. Minimum Moves to Reach Target Score from LeetCode

--- LeetCode/Java/Medium/1303. Minimum Moves to Reach Target Score/Solution.java
+++ LeetCode/Java/Medium/1303. Minimum Moves to Reach Target Score/Solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public int minMoves(int target, int maxDoubles) {
         int count=0;
-        if(maxDoubles == 0) return target-1;
-        if(target==1) return 0;
-        while(target != 0){
+        // if(maxDoubles == 0) return target-1;
+        // if(target==1) return 0;
+        while(target > 1){
             if(target%2==0 && maxDoubles > 0){
                 target=target/2;
-                count++;
                 maxDoubles--;
             }else{
                 target=target-1;
-                count++;
             }
+            count++;
         }
-        int x=target-1;
-        return count+x;
+        return count;
     }
 }
