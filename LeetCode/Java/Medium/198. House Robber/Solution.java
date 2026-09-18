f4d9331b4da5d7d3114521e3a099268a145105c7
anandchowhan-05 <anandnaikkeloth9966@gmail.com>
2026-09-18 10:32:51 +0530

    Add Medium Java Solution for 198. House Robber from LeetCode

--- LeetCode/Java/Medium/198. House Robber/Solution.java
+++ LeetCode/Java/Medium/198. House Robber/Solution.java
@@ -2,6 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         int a=0;
         int b=0;
+        int
         for(int i=0;i<nums.length;i++){
             if(i%2==0) a+=nums[i];
             else b+=nums[i];
